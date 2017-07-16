3242d394962be4c36e1a51193fdfb44fc665b9ae
Alberto <viher_3@hotmail.com>
2017-07-16 02:34:34 +0200

    Login form

--- www/js/app.js
+++ www/js/app.js
@@ -2,8 +2,10 @@ angular.module($appModule, ['ionic', 'ngMap'])
 
 .run(function($ionicPlatform) 
 {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+  $ionicPlatform.ready(function() 
+  {
+    if(window.cordova && window.cordova.plugins.Keyboard) 
+    {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -13,7 +15,8 @@ angular.module($appModule, ['ionic', 'ngMap'])
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if(window.StatusBar) 
+    {
       StatusBar.styleDefault();
     }
 
