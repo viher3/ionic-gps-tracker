8439b07e568f28f5f292ba109f4bb5ba7c98afc1
Alberto <viher_3@hotmail.com>
2017-07-16 17:18:43 +0200

    Login ok

--- www/js/Controller/LoginController.js
+++ www/js/Controller/LoginController.js
@@ -1,5 +1,22 @@
-angular.module($appModule).controller('LoginController', function($scope)
+angular.module($appModule).controller('LoginController', function($scope, $state)
 {
-    
+	$scope.login = {
+		'server_host' 	: 'albertolabs.com',
+		'server_user' 	: 'demo@albertolabs.com',
+		'server_pwd' 	: 'demo'
+	};
+
+	$scope.signin = function()
+	{
+		if( $scope.login.server_host.length && $scope.login.server_user.length && $scope.login.server_pwd.length )
+		{
+			// go to home page
+			$state.go('index');
+		}
+		else
+		{
+			alert('All fields are required!');
+		}
+	}
 
 });
