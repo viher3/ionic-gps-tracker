38be2dc792876704d4daf191037db36cb5b901c0
Alberto <viher_3@hotmail.com>
2017-07-16 16:47:18 +0200

    stateProvider ok

---
+++ www/js/Controller/LoginController.js
@@ -0,0 +1,5 @@
+angular.module($appModule).controller('LoginController', function($scope)
+{
+    
+
+});
--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,35 @@
-angular.module($appModule, ['ionic', 'ngMap'])
+angular.module($appModule, ['ionic'])
 
 .run(function($ionicPlatform) 
 {
-  $ionicPlatform.ready(function() 
-  {
-    if(window.cordova && window.cordova.plugins.Keyboard) 
-    {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) 
-    {
+
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-
   });
-  
+})
+
+.config(function ($stateProvider, $urlRouterProvider) 
+{
+    $stateProvider
+
+        .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: 'LoginController'
+        })
+
+        .state('index', {
+            url: '/index',
+            templateUrl: 'templates/home.html',
+            controller: 'IndexController'
+        })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+
 });
